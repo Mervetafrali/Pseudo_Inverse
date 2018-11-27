28518be7fb348d0975da416a7dcb791e6fb767ed
MERVE <mervetafralii@gmail.com>
2018-11-27 22:19:16 +0300

    3*2,3*4

--- PseudoInvese/PseudoInvese/Form1.Designer.cs
+++ PseudoInvese/PseudoInvese/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -53,7 +55,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(428, 147);
+            this.label3.Location = new System.Drawing.Point(641, 127);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 17);
             this.label3.TabIndex = 2;
@@ -61,16 +63,35 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(594, 146);
+            this.label4.Location = new System.Drawing.Point(457, 117);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(0, 17);
             this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(457, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(516, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Det: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -89,6 +110,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- PseudoInvese/PseudoInvese/Form1.cs
+++ PseudoInvese/PseudoInvese/Form1.cs
@@ -12,42 +12,46 @@ namespace PseudoInvese
 {
     public partial class Form1 : Form
     {
-       
-            public Form1()
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Random random = new System.Random();
+
+            int satir = 3;
+            int sutun = 4;
+
+            while (satir == sutun)
             {
-                InitializeComponent();
+                sutun = random.Next(1, 5);
             }
 
-            private void Form1_Load(object sender, EventArgs e)
+            double[,] matris = new double[satir, sutun];
+            double[,] matrist = new double[satir, sutun];
+            double[,] matrisc = new double[satir, sutun];
+            double[,] matrisdi = new double[satir, sutun];
+            //rastgele sayi atama
+            for (int i = 0; i < satir; i++)
             {
-                System.Random random = new System.Random();
-
-                int satir = random.Next(1, 5);
-                int sutun = random.Next(1, 5);
-
-                while (satir == sutun)
+                for (int j = 0; j < sutun; j++)
                 {
-                    sutun = random.Next(1, 5);
+                    matris[i, j] = random.Next(1, 9);
                 }
-
-                double[,] matris = new double[satir, sutun];
-                double[,] matrist = new double[satir, sutun];
-                double[,] matrisc = new double[satir, sutun];
-                for (int i = 0; i < satir; i++)
+            }
+            for (int i = 0; i < satir; i++)
+            {
+                for (int j = 0; j < sutun; j++)
                 {
-                    for (int j = 0; j < sutun; j++)
-                    {
-                        matris[i, j] = random.Next(1, 9);
-                    }
+                    label1.Text += matris[i, j] + " ";
                 }
-                for (int i = 0; i < satir; i++)
-                {
-                    for (int j = 0; j < sutun; j++)
-                    {
-                        label1.Text += matris[i, j] + " ";
-                    }
-                    label1.Text += "\n";
-                }
+                label1.Text += "\n";
+            }
+            if (satir < sutun)
+            {
                 matrist = Transpose(matris);
                 int k = matrist.GetLength(0);
                 int l = matrist.GetLength(1);
@@ -70,50 +74,207 @@ namespace PseudoInvese
                     }
                     label4.Text += "\n";
                 }
+                if (matrisc.GetLength(0) == 2 && matrisc.GetLength(1) == 2)
+                {
+                    matrisdi = inverse_2(matrisc);
+                    for (int i = 0; i < 2; i++)
+                    {
+                        for (int j = 0; j < 2; j++)
+                        {
+                            label3.Text += matrisdi[i, j] + " ";
+                        }
+                        label3.Text += "\n";
+                    }
+
+                    double[,] inverse = new double[sutun, satir];
+                    inverse = multiplication(matrist, matrisdi);
+                    int y = inverse.GetLength(0);
+                    int f = inverse.GetLength(1);
+                    for (int i = 0; i < y; i++)
+                    {
+                        for (int j = 0; j < f; j++)
+                        {
+                            label5.Text += inverse[i, j] + " ";
+                        }
+                        label5.Text += "\n";
+                    }
+                }
+                else if (matrisc.GetLength(0) == 3 && matrisc.GetLength(1) == 3)
+                {
+                    matrisdi = inverse_3(matrisc);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            label3.Text += matrisdi[i, j] + " ";
+                        }
+                        label3.Text += "\n";
+                    }
+
+                    double[,] inverse = new double[sutun, satir];
+                    inverse = multiplication(matrist, matrisdi);
+                    int y = inverse.GetLength(0);
+                    int f = inverse.GetLength(1);
+                    for (int i = 0; i < y; i++)
+                    {
+                        for (int j = 0; j < f; j++)
+                        {
+                            label5.Text += inverse[i, j] + " ";
+                        }
+                        label5.Text += "\n";
+                    }
+
+                }
             }
-
-
-            public double[,] Transpose(double[,] matrix)
+            else if (satir > sutun)
             {
-                int w = matrix.GetLength(0);
-                int h = matrix.GetLength(1);
-
-                double[,] transpoz = new double[h, w];
-
-                for (int i = 0; i < w; i++)
+                matrist = Transpose(matris);
+                int k = matrist.GetLength(0);
+                int l = matrist.GetLength(1);
+                for (int i = 0; i < k; i++)
+                {
+                    for (int j = 0; j < l; j++)
+                    {
+                        label2.Text += matrist[i, j] + " ";
+                    }
+                    label2.Text += "\n";
+                }
+                matrisc = multiplication(matrist, matris);
+                int g = matrisc.GetLength(0);
+                int h = matrisc.GetLength(1);
+                for (int i = 0; i < g; i++)
                 {
                     for (int j = 0; j < h; j++)
                     {
-                        transpoz[j, i] = matrix[i, j];
+                        label4.Text += matrisc[i, j] + " ";
                     }
+                    label4.Text += "\n";
                 }
-
-                return transpoz;
-            }
-            public double[,] multiplication(double[,] matrix, double[,] matrix2)
-            {
-                int s = matrix.GetLength(0);
-                int sut = matrix.GetLength(1);
-                int s2 = matrix2.GetLength(0);
-                int sut2 = matrix2.GetLength(1);
-                double[,] c = new double[s, sut2];
-
-
-                for (int i = 0; i < s; i++)
+                if (matrisc.GetLength(0) == 2 && matrisc.GetLength(1) == 2)
                 {
-                    for (int j = 0; j < sut2; j++)
+                    matrisdi = inverse_2(matrisc);
+                    for (int i = 0; i < 2; i++)
                     {
-                        c[i, j] = 0;
-                        for (int k = 0; k < s2; k++) c[i, j] = c[i, j] + matrix[i, k] * matrix2[k, j];
+                        for (int j = 0; j < 2; j++)
+                        {
+                            label3.Text += matrisdi[i, j] + " ";
+                        }
+                        label3.Text += "\n";
                     }
+
+                }
+                double[,] inverse = new double[sutun, satir];
+                inverse = multiplication(matrisdi, matrist);
+                int y = inverse.GetLength(0);
+                int f = inverse.GetLength(1);
+                for (int i = 0; i < y; i++)
+                {
+                    for (int j = 0; j < f; j++)
+                    {
+                        label5.Text += inverse[i, j] + " ";
+                    }
+                    label5.Text += "\n";
                 }
 
-
-
-                return c;
 
             }
         }
+
+
+        public double DET(double[,] a, int n)
+        {
+            int i, j, k;
+            double det = 0;
+            for (i = 0; i < n - 1; i++)
+            {
+                for (j = i + 1; j < n; j++)
+                {
+                    det = a[j, i] / a[i, i];
+                    for (k = i; k < n; k++)
+                        a[j, k] = a[j, k] - det * a[i, k]; // HERE
+                }
+            }
+            det = 1;
+            for (i = 0; i < n; i++)
+                det = det * a[i, i];
+            return det;
+        }
+        public double[,] Transpose(double[,] matrix)
+        {
+            int w = matrix.GetLength(0);
+            int h = matrix.GetLength(1);
+
+            double[,] transpoz = new double[h, w];
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    transpoz[j, i] = matrix[i, j];
+                }
+            }
+
+            return transpoz;
+        }
+        public double[,] multiplication(double[,] matrix, double[,] matrix2)
+        {
+            int s = matrix.GetLength(0);
+            int sut = matrix.GetLength(1);
+            int s2 = matrix2.GetLength(0);
+            int sut2 = matrix2.GetLength(1);
+            double[,] c = new double[s, sut2];
+
+
+            for (int i = 0; i < s; i++)
+            {
+                for (int j = 0; j < sut2; j++)
+                {
+                    c[i, j] = 0;
+                    for (int k = 0; k < s2; k++) c[i, j] = c[i, j] + matrix[i, k] * matrix2[k, j];
+                }
+            }
+
+
+
+            return c;
+
+        }
+        public double[,] inverse_2(double[,] matrix)
+        {
+            double[,] matrix2 = new double[2, 2]{ { matrix[1, 1] , -1 * matrix[0, 1] }, { -1 * matrix[1, 0] , matrix[0, 0] } };
+            double det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            double[,] matrix3 = new double[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix3[i, j] = (1 / det) * matrix2[i, j];
+                }
+                label3.Text += "\n";
+            }
+            return matrix3;
+        }
+        public double[,] inverse_3(double[,] matrix)
+        {
+            double[,] matrix2 = new double[3, 3] { { matrix[1,1]*matrix[2,2]-matrix[1,2]*matrix[2,1], matrix[0, 2]*matrix[2, 1] - matrix[0,1] * matrix[2, 2] , matrix[0, 1] * matrix[1, 2] - matrix[0, 2] * matrix[1, 1] },
+                                                   { matrix[1,2]*matrix[2,0]-matrix[1,0]*matrix[2,2], matrix[0, 0]*matrix[2, 2] - matrix[0,2] * matrix[2, 0] , matrix[0, 2] * matrix[1, 0] - matrix[0, 0] * matrix[1, 2] },
+                                                   { matrix[1,0]*matrix[2,1]-matrix[1,1]*matrix[2,0], matrix[0, 1]*matrix[2, 0] - matrix[0,0] * matrix[2, 1] , matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0] }};
+
+            double[,] matrix3 = new double[3,3];
+            double a = DET(matrix, matrix.GetLength(0));
+            label6.Text += a;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix3[i, j] = (1 / a) * matrix2[i, j];
+                }
+                label3.Text += "\n";
+            }
+            return matrix3;
+            
+        }
     }
+}
 
 
